105aadd57fcaf004d89f3875c8f93d344b9d4742
Arucard1983 <nosferatu.arucard.1983@gmail.com>
2019-07-04 17:21:26 +0100

    Change version strings
    To fix the previous commit

--- src/R33Library.cpp
+++ src/R33Library.cpp
@@ -209,7 +209,7 @@ bool R33File::IBANDLFileSave(wxGrid *tableDataEditor, wxTextCtrl* textEditElemen
  file.AddLine( wxEmptyString);
  file.AddLine( wxT("Version: R33") );
  file.AddLine( wxT("Source: ERYA") );
- file.AddLine( wxT("Name: ERYA Profiling v2.50") );
+ file.AddLine( wxT("Name: ERYA Profiling v2.60") );
  file.AddLine( wxT("Address1:") );
  file.AddLine( wxT("SubFile:" ) );
  file.AddLine( wxT("Serial Number:") );
